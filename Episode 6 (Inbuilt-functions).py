37b7703017345fc6357c20f3c5fe21a6db653f05
amriteshbanerjee2413 <67334509+amriteshbanerjee2413@users.noreply.github.com>
2021-03-21 19:53:03 +0400

    Create Episode 6 (Inbuilt-functions).py

---
+++ Episode 6 (Inbuilt-functions).py
@@ -0,0 +1,31 @@
+abs(-7)
+bin(2)
+complex()
+dict()
+dict = {5, 5, 9}
+num1 = float(input("First Number?: "))
+op = input("Operator: ")
+num2 = float(input("Second Number?: "))
+
+if op == "+":
+     print(num1 + num2)
+elif op == "-":
+     print(num1 - num2)
+elif op == "/":
+     print(num1 / num2)
+elif op == "*":
+     print(num1 * num2)
+else:
+     print("Invalid Operator")
+    
+hash(5)
+help()
+len('supercalifragilisticexpialidocious')
+
+max(4, 5, 6, 7) or min (4, 5, 6, 7)
+random_list = [5, 7, 892, 32, -1]
+min(random_list)
+
+pow(4, 2)
+type(5.5)
+type("2.5")
